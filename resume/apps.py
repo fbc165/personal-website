d5633b96ed6072977a904d93dcc8761c7ede4090
Fabricio <fabricio.alves2698@gmail.com>
2025-04-06 21:51:38 -0300

    fix: config do app resume

--- resume/apps.py
+++ resume/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CurriculumConfig(AppConfig):
+class ResumeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "resume"
