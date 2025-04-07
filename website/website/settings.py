3a021bda9891bd89793c46246a33ba4bb30943f9
Fabricio <fabricio.alves2698@gmail.com>
2025-04-06 23:21:14 -0300

    fix: reset changes

--- website/website/settings.py
+++ website/website/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
