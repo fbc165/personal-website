4d9b20ae21cdfbd78322ecb51496772edaee7f8a
Fabricio Alves <fabricio.alves2698@gmail.com>
2025-04-09 23:11:39 +0000

    fix: entrypoint format

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,5 @@
-# entrypoint.sh
 #!/bin/sh
 
-echo "🚀 Esperando o banco ficar pronto..."
-sleep 10
-
-echo "📦 Rodando migrações..."
-python manage.py migrate --noinput
-
-echo "🧹 Coletando arquivos estáticos..."
+python manage.py migrate
 python manage.py collectstatic --noinput
-
-echo "🔥 Iniciando o Gunicorn..."
-gunicorn personal_website.wsgi:application \
-    --bind 0.0.0.0:8000 \
-    --workers 3 \
-    --timeout 120
+gunicorn website.wsgi:application --bind 0.0.0.0:8000 --workers 5 --timeout 120
